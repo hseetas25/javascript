50c862b5fb948b4d53e0a06dbf160235cdaf1a0a
hseetas25 <balagasateeshspis@gmail.com>
2021-03-05 23:06:24 +0530

    Updated

--- jscript.js
+++ jscript.js
@@ -2,4 +2,50 @@
 var x=5;
 var y=5;
 var z=x+y;
-//let and const keywords in ES6 version
+//assignment operator
+x=x+5;
+y=y*5;
+
+//data types
+
+var num=5;
+var str="string";
+var num_string="12345678";
+
+//declarations
+//1----->
+var clgName;
+clgName="VNRVJIET";
+//2------>
+var collegeName="VNRVJIET";
+
+//Multiple declarations
+
+var empId=500,empName="Abc",emppAge="20";
+
+var a;//undefined
+
+//Numerical and string concatenation
+
+var concat="1"+1+1;
+
+//var has no block level scope
+{
+    var js_var=5;//js_var can be accessed anywhere in this program.
+}
+
+//ES2015 with const and let keyword with block level scope
+
+//let has block level scope
+{
+    let js_let=5;
+}
+
+const pi;
+pi=3.14;
+
+
+
+
+
+
