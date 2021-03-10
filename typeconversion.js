4d484cd2c7b9f615b39f1f2ec4ee0e28ae860d02
hseetas25 <balagasateeshspis@gmail.com>
2021-03-10 18:45:33 +0530

    Added data

---
+++ typeconversion.js
@@ -0,0 +1,11 @@
+//type conversion
+var x=12345
+console.log(x)
+console.log(x.toString())
+var y="5";
+var z=+y;
+console.log(y,z);
+console.log(Number("12345"))
+console.log(Number(false),Number(true));
+d=new Date();
+console.log(Number(d));
